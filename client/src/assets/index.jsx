6e301def3084905f879eb267a49a6482fb628e06
Boun13a <halbounicd@gmail.com>
2022-11-21 18:00:11 -0500

    adding the hospital/logout/signup png logo

--- client/src/assets/index.jsx
+++ client/src/assets/index.jsx
@@ -1,2 +1,6 @@
 export { AddChannel } from './AddChannel';
 export { CloseCreateChannel } from './CloseCreateChannel';
+export { ChannelInfo } from './ChannelInfo';
+export { InviteIcon } from './InviteIcon';
+export { LightningBolt } from './LightningBolt';
+export { SearchIcon } from './SearchIcon';
