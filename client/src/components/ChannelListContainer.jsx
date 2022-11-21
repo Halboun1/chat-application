19a669115898efb4393c15d351b3ceb8e2045094
Boun13a <halbounicd@gmail.com>
2022-11-21 18:53:14 -0500

    adding more css styling

--- client/src/components/ChannelListContainer.jsx
+++ client/src/components/ChannelListContainer.jsx
@@ -3,8 +3,8 @@ import { ChannelList, useChatContext } from 'stream-chat-react';
 import { ChannelSearch, TeamChannelList, TeamChannelPreview } from './';
 import Cookies from 'universal-cookie'; 
 
-import HospitalIcon from '../assets/hospital.png';
-
+import HospitalIcon from '../assets/hospital1.png';
+import LogoutIcon from '../assets/logout.png';
 
 
 const SideBar = () => (
@@ -12,6 +12,11 @@ const SideBar = () => (
         <div className="channel-list__sidebar__icon1">
             <div className="icon1__inner">
                 <img  src={HospitalIcon} alt="Hospital" width="30"/>
+            </div>
+        </div>
+        <div className="channel-list__sidebar__icon2">
+            <div className="icon1__inner">
+                <img  src={LogoutIcon} alt="Logout" width="30"/>
             </div>
         </div>
     </div>
@@ -22,7 +27,9 @@ const SideBar = () => (
 
 const channelListContainer = () => {
   return (
-    <div>channelListContainer</div>
+    <>
+      <SideBar/>
+    </>
   )
 }
 
