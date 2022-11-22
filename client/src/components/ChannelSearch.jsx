0f21cc5b074ee66e4c21825c1db62f30c329d752
Boun13a <halbounicd@gmail.com>
2022-11-21 19:55:25 -0500

    creating Auth componnent for signing in or signing up users

--- client/src/components/ChannelSearch.jsx
+++ client/src/components/ChannelSearch.jsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { useChatContext } from 'stream-chat-react';
+import { getChannel, useChatContext } from 'stream-chat-react';
 import { SearchIcon } from '../assets';
 
 const ChannelSearch = () => {
@@ -7,11 +7,20 @@ const ChannelSearch = () => {
     const [query, setQuery ] = useState('');
     const [ loading, setLoading] = useState(false);
 
+    const getChannels = async (text) => {
+        try{
+            //TODO: fetch channels
+        } catch (error) {
+            setQuery('')
+        }
+    }
+
     const onSearch = (event) => {
         event.preventDefault();
 
         setLoading(true);
         setQuery(event.target.value);
+        getChannels(event.target.value);
     }
     
   return (
