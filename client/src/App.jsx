d3f567450562a8e30a65728cd17e8258b0a03eba
Boun13a <halbounicd@gmail.com>
2022-12-12 19:52:37 -0500

    updating all user info

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,15 +7,32 @@ import Cookies from 'universal-cookie';
 
 import { ChannelListContainer, ChannelContainer, Auth } from './components';
 
+
+import 'stream-chat-react/dist/css/index.css';
 import './App.css';
 
+const cookies = new Cookies();
 const apiKey = '7fhntkmurajh';
+const authToken = cookies.get(apiKey);
 const client = StreamChat.getInstance(apiKey);
 
-const authToken = false;
 
+if(authToken) {
+  client.connectUser({
+    id: cookies.get('userId'),
+    name: cookies.get('username'),
+    fullName: cookies.get('fullName'),
+    image: cookies.get('avatarURL'),
+    hashedPassword: cookies.get('hashedPassword'),
+    phoneNumber: cookies.get('phoneNumber'),
+  },authToken)
+}
 
 const App = () => {
+
+  const [createType, setCreateType] = useState('');
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   if(!authToken) return <Auth />
   
   return (
